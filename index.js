153a59dff604a6e66ab4ccd61574406b23a9a001
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-10-26 20:30:48 +0600

    serser side data added

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const courses = require('./courses.json')
+const slider = require('./courses.json')
+const app = express()
+app.use(cors())
+const port = 5000
+
+
+
+app.get('/', (req, res) => {
+    res.send('courses server!')
+})
+
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/slider', (req, res) => {
+    res.send(slider);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const course = courses.find(c => c.id === id) || {};
+    res.send(course);
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
